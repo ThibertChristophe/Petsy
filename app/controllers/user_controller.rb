1294a1fba24f9e47570e2ee3152d45333734fa74
ThibertChristophe <c.thibert89@gmail.com>
2024-01-14 00:27:53 +0100

    Validation des password avec has_secure_password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,5 +4,20 @@ class UserController < ApplicationController
   end
 
   def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = "Utilisateur créé avec succès"
+      # redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Permet de valider les parametres
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "user#new"
-  get "up" => "rails/health#show", :as => :rails_health_check
   resources :user, only: %i[new create]
 end
