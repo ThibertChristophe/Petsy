cf2338e8168935ac73bb4d6a6501b15eaad3548a
ThibertChristophe <c.thibert89@gmail.com>
2024-01-13 22:53:58 +0100

    first commit

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Petsy
+  class Application < Rails::Application
+    config.load_defaults 7.1
+
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+  end
+end
