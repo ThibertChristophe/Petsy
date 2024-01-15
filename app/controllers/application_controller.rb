7e5cd7e84c4b3c073b44c1731b2e3694e7293358
ThibertChristophe <c.thibert89@gmail.com>
2024-01-15 23:31:03 +0100

    Ajout d'une fonction de verif si on est authentifié ou pas, ajout au niveau du controller application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def only_signed_in
+    return unless !session[:auth] || !session[:auth]['id']
+
+    flash[:danger] = "Vous n'avez pas les droits d'accéder à cette page"
+    redirect_to :new
+  end
 end
