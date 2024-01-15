8fb15b7206e8c4ef2aeaff55738ab1047796b0e6
ThibertChristophe <c.thibert89@gmail.com>
2024-01-15 22:46:08 +0100

    Confirmation du user avec email et page de confirmation qui verifie le token + id dans le session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[confirm]
+
   def new
     @user = User.new
   end
@@ -7,7 +9,8 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      flash[:success] = 'Utilisateur créé avec succès'
+      flash[:success] = 'Utilisateur créé avec succès, veuillez vérifier vos emails'
+      # envoi le mail
       UserMailer.confirm(@user).deliver_now
       redirect_to root_path
     else
@@ -15,7 +18,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def confirm
+    # si le parametre token dans l url correspond bien au token dans la db pour ce user
+    # ps : on a recup l id avec le before_action qui set le @user
+    if @user.confirmation_token == params[:token]
+      @user.confirm = true
+      @user.confirmation_token = nil
+      @user.save validate: false
+      session[:auth] = { id: @user.id }
+      flash.clear
+      @result = 'Utilisateur confirmé, vous pouvez vous connecter'
+    else
+      @result = 'Validation impossible'
+    end
+  end
+
   private
+
+  # Permet d eviter de repeter cette ligne, on va l utiliser dans le before_action
+  # Recup du param id dans url
+  def set_user
+    @user = User.find params[:id]
+  end
 
   # Permet de valider les parametres
   def user_params
