584124fd3130a234da8918a5e6904303ae5e8505
ThibertChristophe <c.thibert89@gmail.com>
2024-01-28 23:04:12 +0100

    Ajout du formulaire d'ajout d'un animal pour le user connecté

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,11 +8,26 @@ class PetsController < ApplicationController
     @pet = current_user.pets.new
   end
 
-  def create; end
+  def create
+    @pet = current_user.pets.new pet_param
+    if @pet.save
+      flash[:success] = 'Animal ajouté'
+      redirect_to pets_path
+    else
+      flash[:danger] = 'Error'
+      redirect_to new_pet_path
+    end
+  end
 
   def update; end
 
   def destroy; end
 
   def edit; end
+
+  private
+
+  def pet_param
+    params.require(:pet).permit(:name, :birthday, :gender, :species_id)
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,14 @@
 class Pet < ApplicationRecord
   belongs_to :user
   belongs_to :species
+
+  validates :name, :gender, :birthday, presence: true
+  validates :gender, format: { with: /\A(M|F)\z/ }
+  validate :birthday_not_future
+
+  def birthday_not_future
+    return unless birthday.present? && birthday.future?
+
+    errors.add :birthday, 'ne peut être dans le futur'
+  end
 end
